0625fd864e4248c4694a084a0faf98672e678115
Numan <52382803+ozkayanuman@users.noreply.github.com>
2020-02-07 05:58:44 +0300

    last update
    adding commends

--- Assets/Scripts/SuccesScript.cs
+++ Assets/Scripts/SuccesScript.cs
@@ -15,16 +15,17 @@ public class SuccesScript : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.tag = "Succes";
-            collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            collision.gameObject.GetComponent<CarController>().enabled = false;
-            collision.gameObject.AddComponent<LevelReloader>();
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;       //making car speed zero
+            collision.gameObject.GetComponent<CarController>().enabled = false;             //disable input for this car
+            collision.gameObject.AddComponent<LevelReloader>();                             //making this car obstacles
             GameController.gameController.stage++;
 
             DontDestroyOnLoad(collision.gameObject);
-            GameController.gameController.successedCars.Add(collision.gameObject);
+
+            GameController.gameController.successedCars.Add(collision.gameObject);          //adding this car to parked car list to remove new scene 
 
 
-            if (GameController.gameController.stage > 1)    //Determines when the EndLevelPanel is displayed 
+            if (GameController.gameController.stage > 8)    //Determines when the EndLevelPanel is displayed 
             {
                 GameObject myUIManagerGameObject = GameObject.FindGameObjectWithTag("UserInterface");
                 myUIManagerGameObject.GetComponent<UIController>().ShowEndLevelPanel();
